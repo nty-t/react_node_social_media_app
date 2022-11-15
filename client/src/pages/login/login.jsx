99ca7750d06177c799d8eb9c6b50473fe428a4b2
nty <nathan11002@gmail.com>
2022-11-15 12:17:17 +0300

    project initiation and created login page with sass css as well as added .gitignore

---
+++ client/src/pages/login/login.jsx
@@ -0,0 +1,30 @@
+import "./login.scss";
+const login = () => {
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <span>Don't have an Account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="UserName" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default login;
---
+++ client/src/pages/register/register.jsx
@@ -0,0 +1,7 @@
+import "./register.scss";
+
+const register = () => {
+  return <div className="register">register</div>;
+};
+
+export default register;
