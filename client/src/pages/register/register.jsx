956dd05c1fbad2f05a6277fd3151040785fe777b
nty <nathan11002@gmail.com>
2022-11-15 14:41:57 +0300

    updated register page

--- client/src/pages/register/register.jsx
+++ client/src/pages/register/register.jsx
@@ -1,7 +1,33 @@
 import "./register.scss";
 
-const register = () => {
-  return <div className="register">register</div>;
+const Register = () => {
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>My Social.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
+            alias totam numquam ipsa exercitationem dignissimos, error nam,
+            consequatur.
+          </p>
+          <span>Do you have an account?</span>
+
+          <button>Login</button>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="text" placeholder="Name" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default register;
+export default Register;
